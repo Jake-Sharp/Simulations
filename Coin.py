c2a970024e3595debf3847486c344b109ebb6fcd
Jacob Sneed <jake sharp>
2020-02-12 08:45:39 -0600

    Add coin.py

---
+++ Coin.py
@@ -0,0 +1,22 @@
+import random
+roll = random.randint(1,2)
+
+count1
+
+
+x = 0
+flipNum = input("How many coins would you like to flip? ")
+
+while(x <= flipNum):
+    flip = random.randint(1,6)
+    print(roll)
+    x += 1
+    if(roll = 1):
+        count1 +=1
+    elif(roll = 2):
+        count2 +=1
+
+
+        print(f"Flip = Tails {count1} / {flipNum} = {(count1/flipNum)*100}% " times!")
+        print(f"Flip = Heads {count2} / {flipNum} = {(count1/flipNum)*100}% " times!")
+       
