2b87ceb00bf659caedbe0872f5277c0dbc309e72
Jacob Sneed <jake sharp>
2020-02-12 08:26:37 -0600

    Add dice.py

---
+++ dice.py
@@ -0,0 +1,38 @@
+import random
+roll = random.randint(1,6)
+
+count1
+count2
+count3
+count4
+count5
+count6
+
+x = 0
+x = input("How many dice would you like to roll?(1-100): ")
+
+while(x <= rollNum):
+    roll = random.randint(1,6)
+    print(roll)
+    x += 1
+    if(roll = 1):
+        count1 +=1
+    elif(roll = 2):
+        count2 +=1
+    elif(roll = 3):
+        count3 +=1
+    elif(roll = 4):
+        count4 +=1
+    elif(roll = 5):
+        count5 +=1
+    elif(roll = 6):
+        count6 +=1
+
+        print(f"Dice = 1 {count1} / {rollNum} = {(count1/rollNum)*100}% " times!")
+        print(f"Dice = 1 {count2} / {rollNum} = {(count1/rollNum)*100}% " times!")
+        print(f"Dice = 1 {count3} / {rollNum} = {(count1/rollNum)*100}% " times!")
+        print(f"Dice = 1 {count4} / {rollNum} = {(count1/rollNum)*100}% " times!")
+        print(f"Dice = 1 {count5} / {rollNum} = {(count1/rollNum)*100}% " times!")
+        print(f"Dice = 1 {count6} / {rollNum} = {(count1/rollNum)*100}% " times!")                             
+    
+    
