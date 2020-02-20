bca451f6123246e28e495d90e012af19f9a187bb
Jacob Sneed <jake sharp>
2020-02-20 09:44:12 -0600

    add sort

--- Coin.py
+++ Coin.py
@@ -1,22 +1,34 @@
 import random
-roll = random.randint(1,2)
 
-count1
+numFlips = int(input("How many flips? "))
 
+flips = [0] * numFlips
+heads = 0
+tails = 0
+runsFound = 0
 
-x = 0
-flipNum = input("How many coins would you like to flip? ")
+for i in range(numFlips):
+    flip = random.randint(0,1)
+    flips[i] += flip
 
-while(x <= flipNum):
-    flip = random.randint(1,6)
-    print(roll)
-    x += 1
-    if(roll = 1):
-        count1 +=1
-    elif(roll = 2):
-        count2 +=1
+for i in range(len(flips)):
+    if(flips[i] == 1):
+        tails+=1
+    elif(flips[i] == 0):
+        heads+=1
 
+print(f"{heads} Heads {heads/numFlips*100}%, {tails} Tails {tails/numFlips*100}%")
 
-        print(f"Flip = Tails {count1} / {flipNum} = {(count1/flipNum)*100}% " times!")
-        print(f"Flip = Heads {count2} / {flipNum} = {(count1/flipNum)*100}% " times!")
-       
+#flips = [0,0,0,1,1,1,0,0,1,1,0,1]
+lookFor = int(input("Which side are you looking for (1-tails, 0-heads)? "))
+if(lookFor == 1):
+    notSearch = 0
+elif(lookFor == 0):
+    notSearch = 1
+run = int(input("How long is the run you're looking for? "))
+for i in range(len(flips)):
+    if(flips[i:i+run+1] == [lookFor]*run+[notSearch]):
+        runsFound+=1
+
+print(flips[0:numFlips])
+print(f"Heads shows up {run} times in a row, {runsFound} times")
